a3a7ef7b80dcf3fdad891fed7c91c1894f1bf6ce
rajin mansuri <rajin.m@upsquare.in>
2023-12-07 16:35:08 +0530

    add new frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import {BrowserRouter,Routes,Route} from "react-router-dom"
+import Layout from './component/Layout'
+import Home from './pages/Home'
+import About from './pages/About'
+
+
+const App = () => {
+  return (
+       <>
+        <BrowserRouter>
+          <Routes>
+          <Route path='/' element={<Layout/>}>
+            <Route index element={<Home/>} />
+            <Route path='/about' element={<About/>} />
+          </Route>
+          </Routes>
+        </BrowserRouter>
+    
+      </>
+  )
+}
+
+export default App
